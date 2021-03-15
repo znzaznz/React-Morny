f333199c0fcffa9cd9d4b7aaf966bf6efbfb30de
xiao <779740337@qq.com>
2021-03-15 19:28:23 +0800

    numberPad的ui完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,33 +3,63 @@ import Layout from '../components/Layout';
 import styled from 'styled-components';
 import TagsSection from '../components/TagsSection';
 import NoteSection from '../components/NoteSection';
+import CategorySection from "../components/CategorySection/index"
 
 
-const CategorySection = styled.section`
-    font-size: 24px;
-    > ul{
-      display: flex;
-      background-color: #c4c4c4;
-      > li{
-        position: relative; 
+
+const NumberPadSection = styled.section`
+  > .output{
+    background: white;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25);
+  }
+  > .pad{
+    > button{
+      background: none;
+      border: none;
+      float: left;
+      width: 25%;
+      height: 64px;
+      &.OK{
+        height: 128px;
+        float: right;
+      }
+      &.zero{
         width: 50%;
-        text-align: center;
-        padding: 16px 0;
-        &.selected::after{
-          content: "";
-          display: block;
-          height: 3px;
-          background-color: #333;
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          width: 100%;
-        }
+      }
+      &:nth-child(1){
+        background:#f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5){
+        background:#E0E0E0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9){
+        background-color: #D3D3D3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10){
+        background-color: #C1C1C1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13){
+        background-color: #B8B8b8;
+      }
+      &:nth-child(12){
+        background-color: #9a9a9a;
+      }
+      &:nth-child(14){
+        background-color: #a9a9a9;
       }
     }
-`
-const NumberPadSection = styled.section`
-
+  }
 `
 
 export default function Money(){
@@ -37,15 +67,10 @@ export default function Money(){
         <Layout>
             <TagsSection/>
             <NoteSection/>
-            <CategorySection>
-                <ul>
-                    <li className={"selected"}>支出</li>
-                    <li>收入</li>
-                </ul>
-            </CategorySection>
+            <CategorySection/>
             <NumberPadSection>
-                <div>100</div>
-                <div>
+                <div className={"output"}>100</div>
+                <div className={"pad clearfix"}>
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
@@ -57,8 +82,8 @@ export default function Money(){
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button>OK</button>
-                    <button>0</button>
+                    <button className={"OK"}>OK</button>
+                    <button className={"zero"}>0</button>
                     <button>.</button>
                 </div>
             </NumberPadSection>
