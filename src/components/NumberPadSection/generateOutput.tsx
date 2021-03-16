3c03e7f69520ecc612412cbb8e06483004b92779
xiao <779740337@qq.com>
2021-03-16 16:24:48 +0800

    完成numberPad数字功能

---
+++ src/components/NumberPadSection/generateOutput.tsx
@@ -0,0 +1,36 @@
+const generateOutput = (text:string,output:string)=>{
+    switch (text) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            if (output === "0"){
+                return text
+            }else{
+                return output + text
+            }
+        case '.':
+            if (output.indexOf(".") >= 0){
+                return output
+            }else{
+                return output + text;
+            }
+        case '删除':
+            if (output.length === 1) {
+                return ('0');
+            } else {
+                return (output.slice(0, -1));
+            }
+        case '清空':
+            return ('0');
+        case 'OK':
+            return ('OK');
+    }
+}
+export {generateOutput}
--- src/components/NumberPadSection/index.tsx
+++ src/components/NumberPadSection/index.tsx
@@ -1,113 +1,29 @@
 import React, {useState} from 'react';
-import styled from "styled-components";
-import {unwatchFile} from 'fs';
-
-const NumberPadSection = styled.section`
-  > .output{
-    background: white;
-    font-size: 36px;
-    line-height: 64px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad{
-    > button{
-      background: none;
-      border: none;
-      float: left;
-      width: 25%;
-      height: 64px;
-      &.OK{
-        height: 128px;
-        float: right;
-      }
-      &.zero{
-        width: 50%;
-      }
-      &:nth-child(1){
-        background:#f2f2f2;
-      }
-      &:nth-child(2),
-      &:nth-child(5){
-        background:#E0E0E0;
-      }
-      &:nth-child(3),
-      &:nth-child(6),
-      &:nth-child(9){
-        background-color: #D3D3D3;
-      }
-      &:nth-child(4),
-      &:nth-child(7),
-      &:nth-child(10){
-        background-color: #C1C1C1;
-      }
-      &:nth-child(8),
-      &:nth-child(11),
-      &:nth-child(13){
-        background-color: #B8B8b8;
-      }
-      &:nth-child(12){
-        background-color: #9a9a9a;
-      }
-      &:nth-child(14){
-        background-color: #a9a9a9;
-      }
-    }
-  }
-`
+import NumberPadSection from './NumberPadSection';
+import {generateOutput} from './generateOutput';
 
 export default function Index() {
-    const [output,setOutput] = useState("0")
-    const onClickNum = (e:React.MouseEvent)=>{
+    const [output, setOutput] = useState('0');
+    const onClickNum = (e: React.MouseEvent) => {
         const text = (e.target as HTMLButtonElement).textContent;
-        if (text === null){return}
-        if (text){
-            switch (text){
-                case "0":
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                    if (output === "0"){
-                        setOutput(text)
-                    }else{
-                        setOutput(output+text)
-                    }
-                    break;
-                case ".":
-                    if (output.indexOf(".") === -1){
-                        console.log(1);
-                        if (output !== "0"){
-                            setOutput(output+text)
-                        }
-                    }
-                    break;
-                case "删除":
-                    console.log('删除');
-                    break;
-                case "清空":
-                    console.log('清空');
-                    break;
-                case "确认":
-                    console.log('确认');
-                    break;
-                case "OK":
-                    console.log('OK');
-                    break;
+        if (text === null) {
+            return;
+        }
+        if (text) {
+            if (output.length >= 16){
+                alert("数字太大了")
+            }else if (output.indexOf(".") !== -1 && output.length - output.indexOf(".") > 2 ){
+                alert("数字输入错误")
+            }else {
+                setOutput(generateOutput(text,output) as string)
             }
         }
-    }
+    };
     return (
         <>
             <NumberPadSection>
-                <div className={"output"}>{output}</div>
-                <div className={"pad clearfix"} onClick={onClickNum}>
+                <div className={'output'}>{output}</div>
+                <div className={'pad clearfix'} onClick={onClickNum}>
                     <button>1</button>
                     <button>2</button>
                     <button>3</button>
@@ -119,11 +35,11 @@ export default function Index() {
                     <button>7</button>
                     <button>8</button>
                     <button>9</button>
-                    <button className={"OK"}>OK</button>
-                    <button className={"zero"}>0</button>
+                    <button className={'OK'}>OK</button>
+                    <button className={'zero'}>0</button>
                     <button>.</button>
                 </div>
             </NumberPadSection>
         </>
-    )
+    );
 }
